455947341de031679c8850f0f8d60f4f01352c22
Vasileios Milias <milias.vasilis@gmail.com>
2021-09-06 16:42:29 +0200

    removed duplicate prints

--- fsq_api_replit.py
+++ fsq_api_replit.py
@@ -12,8 +12,9 @@ client_id=os.environ['client_id'],
 client_secret=os.environ['client_secret'],
 v='20180323',
 ll='52.372473,4.896255',
-query='food',
-radius='300',
+query='[restaurant, food]',
+categoryId = '4bf58dd8d48988d10e941735',
+radius='3000',
 limit=100
 )
 
@@ -25,23 +26,25 @@ res = []
 for v in data["response"]["venues"]:
   tmp = {}
   tmp["name"] = v['name']
+  # print("Name: ",v['name'])
   tmp["cat_id"] = v['id']
+  # print("Categories ID: ", v['id'])
   try:
-      tmp["cat_name"] = v['categories'][0]['name']
+    tmp["cat_name"] = v['categories'][0]['name']
   except:
-      tmp["cat_name"] = 'None'
+    tmp["cat_name"] = 'None'
   tmp["latitutde"] = v['location']['lat']
   tmp["longitude"] = v['location']['lng']
-  print(json.dumps(res, indent=4, sort_keys=True))
+  # print(v['location']['lat'], v['location']['lng'])
   res.append(tmp)
-  print("###############################################\n")
 
+print(json.dumps(res, indent=4, sort_keys=True))
 print("#########################")
 print("# Results retrieved: " + str(len(data["response"]["venues"]))+ " #")
 print("#########################\n")
 
-keys = res[0].keys()
-with open('result.csv', 'w', newline='')  as output_file:
-    dict_writer = csv.DictWriter(output_file, keys)
-    dict_writer.writeheader()
-    dict_writer.writerows(res)
+# keys = res[0].keys()
+# with open('result.csv', 'w', newline='')  as output_file:
+#     dict_writer = csv.DictWriter(output_file, keys)
+#     dict_writer.writeheader()
+#     dict_writer.writerows(res)
