8170d5f90931aa2a39c57dae794be5665fe3d4d3
MiliasV <milias.vasilis@gmail.com>
2021-11-18 18:50:53 +0100

    Update fsq_api_replit.py

--- fsq_api_replit.py
+++ fsq_api_replit.py
@@ -21,30 +21,29 @@ limit=10
 resp = requests.get(url=url, params=params)
 data = json.loads(resp.text)
 
-print(data)
-# res = []
-# for v in data["response"]["venues"]:
-#   tmp = {}
-#   tmp["name"] = v['name']
-#   # print("Name: ",v['name'])
-#   tmp["cat_id"] = v['id']
-#   # print("Categories ID: ", v['id'])
-#   try:
-#     tmp["cat_name"] = v['categories'][0]['name']
-#   except:
-#     tmp["cat_name"] = 'None'
-#   tmp["latitutde"] = v['location']['lat']
-#   tmp["longitude"] = v['location']['lng']
-#   # print(v['location']['lat'], v['location']['lng'])
-#   res.append(tmp)
-#
-# print(json.dumps(res, indent=4, sort_keys=True))
-# print("#########################")
-# print("# Results retrieved: " + str(len(data["response"]["venues"]))+ " #")
-# print("#########################\n")
+res = []
+for v in data["response"]["venues"]:
+  tmp = {}
+  tmp["name"] = v['name']
+  # print("Name: ",v['name'])
+  tmp["cat_id"] = v['id']
+  # print("Categories ID: ", v['id'])
+  try:
+    tmp["cat_name"] = v['categories'][0]['name']
+  except:
+    tmp["cat_name"] = 'None'
+  tmp["latitutde"] = v['location']['lat']
+  tmp["longitude"] = v['location']['lng']
+  # print(v['location']['lat'], v['location']['lng'])
+  res.append(tmp)
 
-# keys = res[0].keys()
-# with open('result.csv', 'w', newline='')  as output_file:
-#     dict_writer = csv.DictWriter(output_file, keys)
-#     dict_writer.writeheader()
-#     dict_writer.writerows(res)
+print(json.dumps(res, indent=4, sort_keys=True))
+print("#########################")
+print("# Results retrieved: " + str(len(data["response"]["venues"]))+ " #")
+print("#########################\n")
+
+keys = res[0].keys()
+with open('result.csv', 'w', newline='')  as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(res)
